7ad43dc9b094fc287e0ae5f699c7aa81c7f9cde1
Timo <timo-de-jong@hotmail.com>
2015-01-19 21:20:07 +0100

    Created api class for modular resource extensions

---
+++ inc/api/api.class.php
@@ -0,0 +1,93 @@
+<?php
+
+require_once 'resources/cso/cso.class.php';
+require_once 'resources/cso/enumeration.class.php';
+require_once 'resources/cso/filter.class.php';
+
+class Api
+{
+    /**
+     * @var array $resources Array with resource objects
+     */
+    private $resources = array();
+
+    /**
+     * Load all the resources we got
+     */
+    public function __construct()
+    {
+        $CSO = new Api_Resources_CSO();
+        $CSO->authenticate('timojong', 'FG4d%!k3hU');
+        $this->addResource('CSO', $CSO);
+    }
+
+    /**
+     * Gets all information from one job
+     *
+     * @param string $id The job id
+     */
+    public function getJob($id)
+    {
+        if(strstr($id, '-'))
+        {
+            $CSO = $this->getResource('CSO');
+            return $CSO->getJob($id);
+        }
+    }
+
+    /**
+     * Gets the jobs from te resources
+     *
+     * @param mixed $filter Array with filters
+     * @return array Jobs
+     */
+    public function getJobs($filter = false)
+    {
+        $jobs = array();
+
+        $CSO = $this->getResource('CSO');
+        array_push($jobs, $CSO->getJobs());
+
+        return $jobs;
+    }
+
+    /**
+     * Gets all the educations we got
+     *
+     * @return array Educations
+     */
+    public function getEducations()
+    {
+        $educations = array();
+
+        $CSO = $this->getResource('CSO');
+        $educations = array_merge($educations, $CSO->enumeration->getEducations());
+
+        return $educations;
+    }
+
+    /**
+     * Adds a resource to our resources
+     *
+     * @param string $name The resource name
+     * @param object $resource The resource that we will add to the other resources we have got
+     */
+    private function addResource($name, $resource)
+    {
+        $this->resources[$name] = $resource;
+    }
+
+    /**
+     * Gets a resource
+     *
+     * @param string $name The resource name
+     * @return mixed The resource object
+     */
+    private function getResource($name)
+    {
+        if(isset($this->resources[$name]))
+        {
+            return $this->resources[$name];
+        }
+    }
+}
