a30fed398c60733acf5777a0f28f9ae84f3ce861
Timo <timo-de-jong@hotmail.com>
2015-01-21 20:42:39 +0100

    Fixed that filter dynamically calls functions, I also added 2 functions that can be called through this way: createEducationFilter and createRegionFilter

--- inc/api/resources/cso/filter.class.php
+++ inc/api/resources/cso/filter.class.php
@@ -2,5 +2,97 @@
 
 class Api_Resources_CSO_Filter
 {
+    /**
+     * Creates the filter array
+     *
+     * @param array $filter Options we want to filter
+     * @return array
+     */
+    public function create($filter)
+    {
+        $_filter = array(
+            '__type__' => 'RemoteJobFilter',
+            'featuresFilter' => array(
+                '__type__' => 'RemoteJobFeaturesFilter',
+                'detailFilter' => array(
+                    '__type__' => 'RemoteJobDetailFilter'
+                )
+            )
+        );
 
+        // loop through each filter and call dynamic function
+        foreach($filter as $key => $value)
+        {
+            $function = 'create'.ucfirst(strtolower($key)).'Filter';
+
+            if(method_exists($this, $function))
+            {
+                $this->{$function}($_filter, $value);
+            }
+        }
+
+        return $_filter;
+    }
+
+    /**
+     * Creates education filter part
+     *
+     * @param array $_filter The filter array
+     * @param array|string $education The education(s)
+     */
+    public function createEducationFilter(&$_filter, $education)
+    {
+        if($education !== null)
+        {
+            if(is_array($education))
+            {
+                foreach($education as $value)
+                {
+                    $_filter['featuresFilter']['detailFilter']['educationLevels'][] = array(
+                        '__type__' => 'EducationLevel',
+                        'code' => $value
+                    );
+                }
+            }
+
+            if(is_string($education))
+            {
+                $education['featuresFilter']['detailFilter']['educationLevels'][] = array(
+                    '__type__' => 'EducationLevel',
+                    'code' => $education
+                );
+            }
+        }
+    }
+
+    /**
+     * Creates regions filter part
+     *
+     * @param array $_filter The filter array
+     * @param array|string $region The region(s)
+     */
+    public function createRegionFilter(&$_filter, $region)
+    {
+        if($region !== null)
+        {
+            if(is_array($region))
+            {
+                foreach($region as $value)
+                {
+                    $_filter['featuresFilter']['detailFilter']['regions'][] = array(
+                        '__type__' => 'Region',
+                        'code' => $value
+                    );
+                }
+            }
+
+            if(is_string($region))
+            {
+                $_filter['featuresFilter']['detailFilter']['regions'][] = array(
+                    '__type__' => 'Region',
+                    'code' => $region
+                );
+            }
+        }
+    }
 }
