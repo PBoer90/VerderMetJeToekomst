9424925a6f09bd7f55101c86e58b2135846fa06c
Koen Hendriks <info@koenhendriks.com>
2015-01-21 09:30:29 +0100

    Created some extra javascript functions for carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,20 @@
-
 var currentSlide = 'choice-welcome';
 var Carousel = $('.carousel');
 var carouselNavigation = $('#slide-navigation');
+var slides = setSlides();
 
-//Init Carousel
+/**
+ * Init Carousel
+ */
 Carousel.carousel({
     interval: 0, //We don't want an automatic changing carousel
     keyboard: false
 });
 
-//Slide listener
+/**
+ * Slide listener
+ */
 $('#mainCarousel').on('slide.bs.carousel', function (slideEvent) {
-    console.log(slideEvent);
     currentSlide = $(slideEvent.relatedTarget).attr('id');
     if(currentSlide == 'choice-welcome'){
         carouselNavigation.addClass('disappear');
@@ -23,8 +26,36 @@ $('#mainCarousel').on('slide.bs.carousel', function (slideEvent) {
     }
 });
 
-//Go back
-$('#slide-navigation').click(function(){
-    Carousel.carousel(0);
+/**
+ * Set slide numbers with names from html
+ * @returns object
+ */
+function setSlides(){
+    var obj = {};
+    $('.carousel-inner .item').each(function(slideNumber, element){
+        obj[$(element).attr('id')] = slideNumber;
+    });
+    return obj;
+}
+
+/**
+ * Got to a slide by number or id/name
+ * @param x int|string
+ */
+function goToSlide(x){
+    if(isInt(x)){
+        Carousel.carousel(x);
+    }else{
+        Carousel.carousel(slides[x]);
+    }
+}
+
+/**
+ * Go back a slide
+ * @TODO go to prev slide instead of first
+ *
+ */
+carouselNavigation.click(function(){
+    goToSlide('choice-welcome');
 });
 
--- js/misc.js
+++ js/misc.js
@@ -2,3 +2,8 @@ $('.shb').click(function(e){
     e.preventDefault();
     window.open('http://serioushomebrew.eu');
 });
+
+//Check if var is an int
+function isInt(n) {
+    return n % 1 === 0;
+}
