b29bdd7b13712909a1ac59a72d9942a9bbf28e9a
Timo <timo-de-jong@hotmail.com>
2015-01-21 21:15:06 +0100

    updated test file

--- tests/api.php
+++ tests/api.php
@@ -7,6 +7,9 @@ require_once '../inc/api/api.class.php';
 
 $api = new Api();
 
-print_r($api->getEducations());
-print_r($api->getJobs());
-print_r($api->getJob('01020-639267297291'));
+
+//print_r($api->getRegions());
+//print_r($api->getEducations());
+//print_r($api->getJobs());
+//print_r($api->getJobs(array('region' => array('BRG.0212'))));
+print_r($api->getJob('cso-01420-542618321542'));
