3ad1bef9e0f8af52cc65bed4548c7fe7ff9a3f05
Timo de Jong <timodejong95@gmail.com>
2015-02-01 11:28:56 +0100

    Added branches

--- inc/api/resources/cso/enumeration.class.php
+++ inc/api/resources/cso/enumeration.class.php
@@ -25,6 +25,34 @@ class Api_Resources_CSO_Enumeration
         'caribisch nederland' => 'BRG.0226'
     );
 
+    private $branches = array(
+        'Administratief/Secretarieel' => 'CVG.02',
+        'Fiscaal' => 'CVG.0602',
+        'Auditing/Accountancy' => 'CVG.0604',
+        'Financieel/Economisch' => 'CVG.06',
+        'ICT' => 'CVG.08',
+        'Facilitair/dienstverlening' => 'CVG.09',
+        'Juridisch' => 'CVG.10',
+        'Medisch/verzorging' => 'CVG.12',
+        'Onderzoek/wetenschap' => 'CVG.14',
+        'Onderwijs/opleiding' => 'CVG.15',
+        'Opslag/vervoer/logistiek' => 'CVG.16',
+        'Personeel en organisatie' => 'CVG.18',
+        'Reclame/communicatie/marketing/PR/voorl.' => 'CVG.20',
+        'Volkshuisvesting' => 'CVG.2202',
+        'Sport/recreatie' => 'CVG.2204',
+        'Sociaal/maatschappelijk/welzijn' => 'CVG.22',
+        'Techniek/productie' => 'CVG.24',
+        'Inkoop/verkoop' => 'CVG.26',
+        'Inkoop' => 'CVG.2602',
+        'Verkoop' => 'CVG.2604',
+        'Agrarisch' => 'CVG.27',
+        'Natuur/milieu' => 'CVG.28',
+        'Stedebouwkundig/ruimtelijke ordening' => 'CVG.29',
+        'Orde/vrede/veiligheid' => 'CVG.31',
+        'Documentatie en Informatievoorziening' => 'CVG.32'
+    );
+
     /**
      * Returns the education code for the given education name
      *
@@ -88,5 +116,33 @@ class Api_Resources_CSO_Enumeration
         }
 
         return $regions;
+    }
+
+    /**
+     * Returns the branch code for the given branch name
+     *
+     * @param string $name The branch name
+     * @return mixed The branch code, if we can find the matching branch else false
+     */
+    public function getBranchCode($name)
+    {
+        if(isset($this->branches[strtolower($name)]))
+        {
+            return $this->branches[strtolower($name)];
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Returns all the branches we got
+     *
+     * @return array Branches
+     */
+    public function getBranches()
+    {
+        return array_keys($this->branches);
     }
 }
